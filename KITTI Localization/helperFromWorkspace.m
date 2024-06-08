fa8ccdec8999bd26840fb903c22d6272f23cbb34
Hazem Mohsen <87466265+Hazem-M-Abdelaziz@users.noreply.github.com>
2024-06-08 04:21:42 +0300

    Delete KITTI Localization/helperFromWorkspace.m

--- KITTI Localization/helperFromWorkspace.m
+++
@@ -1,9 +0,0 @@
-function dataStruct = importFromWorkspace(t,data)
-% Construct the signal structure. Flip the dimension of the data to match
-% the input requirements.
-signalStruct = struct('values',permute(data,[3 2 1]),...
-    'dimensions',[1 3]);
-dataStruct = struct('time',t(:),...
-    'signals',signalStruct);
-end
-
